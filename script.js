d128ea54912d594bb7450434371dd3750749877b
Harlequinxy <104767513+Harlequinxy@users.noreply.github.com>
2023-02-03 11:51:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,82 @@
-/**
-for (var i = 0; i < document.querySelectorAll("button").length; i++) {
+var pianoButtons = document.querySelectorAll("button").length;
+for (var i = 0; i < pianoButtons; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function(){
       var buttonInnerhtml = this.innerHTML;
 
       pianoSounds(buttonInnerhtml);
   });
 }
-**/
+/**
 document.addEventListener("keypress", function(event){
   pianoSounds(event.key);
 });
-
+**/
 function pianoSounds(key){
   switch (key) {
+      
       case "a":
-        var c = new Audio("sounds/C.mp3");
-                c.play();
+      var c = new Audio("sounds/C.mp3");
+      c.play();
       break;
+      
       case "w":
-          var cSharp = new Audio("sounds/C_sharp.mp3");
-                  cSharp.play();
+      var cSharp = new Audio("sounds/C_sharp.mp3");
+      cSharp.play();
       break;
+      
       case "s":
-          var d = new Audio("sounds/D.mp3");
-                  d.play();
+      var d = new Audio("sounds/D.mp3");
+      d.play();
       break;
+      
       case "e":
-          var dSharp = new Audio("sounds/D_sharp.mp3");
-                  dSharp.play();
+      var dSharp = new Audio("sounds/D_sharp.mp3");
+      dSharp.play();
       break;
+      
       case "d":
-          var e = new Audio("sounds/E.mp3");
-                  e.play();
+      var e = new Audio("sounds/E.mp3");
+      e.play();
       break;
+      
       case "f":
-          var f = new Audio("sounds/F.mp3");
-                  f.play();
+      var f = new Audio("sounds/F.mp3");
+      f.play();
       break;
+      
       case "t":
-          var fSharp = new Audio("sounds/F_sharp.mp3");
-                  fSharp.play();
+      var fSharp = new Audio("sounds/F_sharp.mp3");
+      fSharp.play();
       break;
+      
       case "g":
-          var g = new Audio("sounds/G.mp3");
-                  g.play();
+      var g = new Audio("sounds/G.mp3");
+      g.play();
       break;
+      
       case "y":
-          var gSharp = new Audio("sounds/G_sharp.mp3");
-                  gSharp.play();
+      var gSharp = new Audio("sounds/G_sharp.mp3");
+      gSharp.play();
       break;
+      
       case "h":
-          var a = new Audio("sounds/A.mp3");
-                  a.play();
+      var a = new Audio("sounds/A.mp3");
+      a.play();
       break;
+      
       case "u":
-          var aSharp = new Audio("sounds/A_sharp.mp3");
-                  aSharp.play();
+      var aSharp = new Audio("sounds/A_sharp.mp3");
+      aSharp.play();
       break;
+      
       case "j":
-          var b = new Audio("sounds/B.mp3");
-                  b.play();
+      var b = new Audio("sounds/B.mp3");
+      b.play();
       break;
+      
       case "k":
-          var c = new Audio("sounds/C_octave.mp3");
-                  c.play();
+      var c = new Audio("sounds/C_octave.mp3");
+      c.play();
       break;
 
     default: console.log(buttonInnerhtml);
